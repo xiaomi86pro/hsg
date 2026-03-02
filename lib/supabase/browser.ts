26e6e68c7a78db04f3f10f6a851722c3b7eb6f2b
xiaomi86pro <xiaomi86pro@gmail.com>
2026-03-02 14:15:10 +0700

    666

--- lib/supabase/browser.ts
+++ lib/supabase/browser.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr"
-import { Database } from "@/types/database"
+import { Database } from "@/lib/supabase/database"
 
 export const supabaseBrowser =
   createBrowserClient<Database>(
